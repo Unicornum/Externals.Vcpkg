70b2861e7c8999a0528584fa4fb20bffed3f2518
unicornum.verum <unicornum.verum@gmail.com>
2024-11-22 19:19:07 +0400

    [Externals.Vcpkg] v0.0.2
    [Externals.Vcpkg] README: дополнено/исправлено описание.
    [Externals.Vcpkg] Добавление в архив нужных для работы integrate install файлов.
    [Externals.Vcpkg] Externals.props: ссылки на папки библиотек.
    [Externals.Vcpkg] Разделение скрипта сборки библиотек на два отдельных.
    [Externals.Vcpkg] Externals.props: перенастройка ссылок для Android проектов.

---
+++ Build.SDK.cmd
@@ -0,0 +1,11 @@
+@echo off
+
+set PathToResultFile=ExternalsSDK_v0.0.0.0.7z
+set Run7z=.\vcpkg\downloads\tools\7zip-24.08-windows\7za.exe
+
+call Build.cmd
+
+rem Упаковка собранных библиотек в архив SDK
+%Run7z% a "%PathToResultFile%" .\Externals.props -ir!vcpkg\vcpkg.exe -ir!vcpkg\.vcpkg-root -ir!vcpkg\scripts\buildsystems\msbuild -ir!vcpkg\installed\x64-windows-static -ir!vcpkg\installed\arm64-android
+
+pause
--- Build.cmd
+++ Build.cmd
@@ -1,8 +1,9 @@
 @echo off
 
 set ANDROID_NDK_HOME=E:\Android\android-ndk-r16b
-set PathToResultFile=.\..\ExternalsSDK_v0.0.0.0.7z
-set Run7z=.\downloads\tools\7zip-24.08-windows\7za.exe
+
+rem Проверить наличие собранных библиотек
+IF exist .\Vcpkg\installed\x64-windows-static goto EndOfFile
 
 rem Копирование в папку .\Vcpkg\ports папки настройки сборки фейковой библиотеки 
 rem externals-dependencies, единственная задача которой - подтянуть зависимости.
@@ -19,7 +20,5 @@ vcpkg install externals-dependencies:arm64-android
 rem Библиотеки собираются в \packages\...
 rem Результат копируется в \installed\...
 
-rem Упаковка собранных библиотек в архив SDK
-%Run7z% a "%PathToResultFile%" vcpkg.exe .\..\Externals.props -ir!installed\x64-windows-static -ir!installed\arm64-android
-
-pause
+cd..
+:EndOfFile
