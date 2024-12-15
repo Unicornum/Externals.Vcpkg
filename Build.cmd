04f55fa8a6a4b7da1c91131148ce803067edddf8
unicornum.verum <unicornum.verum@gmail.com>
2024-12-15 23:49:16 +0400

    [Externals.Vcpkg] Alicorn: настройка сборки сторонних библиотек.

--- Build.cmd
+++ Build.cmd
@@ -1,24 +1,38 @@
 @echo off
 
-IF "%ANDROID_NDK_HOME%"=="" (
-echo error: not set ANDROID_NDK_HOME
-echo please call first 'setx ANDROID_NDK_HOME path\to\androidndk\root'
-pause
-goto EndOfFile
+if "%ANDROID_NDK_HOME%"=="" (
+  echo error: not set ANDROID_NDK_HOME
+  echo please call first 'setx ANDROID_NDK_HOME path\to\androidndk\root'
+  pause
+  goto EndOfFile
 )
 
-rem Проверить наличие собранных библиотек
-IF exist .\Vcpkg\installed\windows goto EndOfFile
+rem Проверить наличие файл-флага
+rem if exist .\Vcpkg\installed\windows goto EndOfFile
 
 rem Собрать vcpkg
 cd vcpkg
-call bootstrap-vcpkg.bat
+call bootstrap-vcpkg.bat -disableMetrics
 
 cd..
 
 rem Запуск сборки vcpkg.json (x64/ARM64 Debug/Release)
-vcpkg\vcpkg.exe install --triplet "x64-windows-static" --x-install-root=vcpkg\installed\windows
+vcpkg\vcpkg.exe install --triplet "x64-windows-static-md" --x-install-root=vcpkg\installed\windows
+if ERRORLEVEL 1 (
+  echo x64-windows-static-md: error: build failed
+  pause
+  goto EndOfFile
+)
+
 vcpkg\vcpkg.exe install --triplet "arm64-android" --x-install-root=vcpkg\installed\android
+if ERRORLEVEL 1 (
+  echo arm64-android: error: build failed
+  pause
+  goto EndOfFile
+)
+
+rem Удачная сборка, добавить файл-флаг как маркер этого факта
+rem ...
 
 rem Библиотеки собираются в \packages\...
 rem Результат копируется в \installed\...
