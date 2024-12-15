0e08ec785f0b3bc34a56744a01136a3cf576c783
unicornum.verum <unicornum.verum@gmail.com>
2024-12-15 23:49:16 +0400

    [Externals.Vcpkg] v0.0.6
    [Externals.Vcpkg] Alicorn: настройка сборки сторонних библиотек.
    [Externals.Vcpkg] Alicorn: перенастройка сборки сторонних библиотек.
    [Externals.Vcpkg] Alicorn: проверка корректности версии Android NDK.
    [Externals.Vcpkg] Alicorn: подключение библиотек для Android.
    [Externals.Vcpkg] Alicorn: fix: ошибки сборки Release версии под Android.
    [Externals.Vcpkg] Установка маркера успешной сборки.
    [Externals.Vcpkg] Externals.props: ссылки на Alicorn.props и Covellite.props, если они существуют.
    [Externals.Vcpkg] Alicorn: перенастройка сборки библиотек.
    [Externals.Vcpkg] Alicorn: перенастройка сборки Externals SDK.
    [Externals.Vcpkg] Alicorn: fix: ошибки при сборке Android проектов.

--- Build.SDK.cmd
+++ Build.SDK.cmd
@@ -1,11 +1,13 @@
 @echo off
 
 set PathToResultFile=ExternalsSDK_v0.0.0.0.7z
-set Run7z=.\vcpkg\downloads\tools\7zip-24.08-windows\7za.exe
+set Run7z=.\vcpkg\downloads\tools\7zip-24.09-windows\7z.exe
 
 call Build.cmd
 
 rem Упаковка собранных библиотек в архив SDK
-%Run7z% a "%PathToResultFile%" .\Externals.props -ir!vcpkg\vcpkg.exe -ir!vcpkg\.vcpkg-root -ir!vcpkg\scripts\buildsystems\msbuild -ir!vcpkg\installed\windows\x64-windows-static -ir!vcpkg\installed\android\arm64-android
+%Run7z% a "%PathToResultFile%" -ir!Externals -xr!src -xr!tools
+%Run7z% a "%PathToResultFile%" Externals.props
+%Run7z% a "%PathToResultFile%" Externals.Android.props
 
 pause
--- Build.cmd
+++ Build.cmd
@@ -1,26 +1,71 @@
 @echo off
 
-IF "%ANDROID_NDK_HOME%"=="" (
-echo error: not set ANDROID_NDK_HOME
-echo please call first 'setx ANDROID_NDK_HOME path\to\androidndk\root'
-pause
-goto EndOfFile
+rem [20.12.2024] Библиотеки корректно собираются для Vs2022 и Android NDK:
+set EXPECTED_ANDROID_NDK=android-ndk-r26d
+
+if "%ANDROID_NDK_HOME%"=="" (
+  echo error: not set ANDROID_NDK_HOME
+  echo please call first 'setx ANDROID_NDK_HOME path\to\androidndk\root'
+  pause
+  goto EndOfFile
 )
 
-rem Проверить наличие собранных библиотек
-IF exist .\Vcpkg\installed\windows goto EndOfFile
+call :CheckPathToAndroidNdk "%ANDROID_NDK_HOME%"
+if ERRORLEVEL 1 goto EndOfFile
+
+rem Проверить наличие файл-флага
+if exist .\Externals\Success.build goto EndOfFile
 
 rem Собрать vcpkg
 cd vcpkg
-call bootstrap-vcpkg.bat
+call bootstrap-vcpkg.bat -disableMetrics
 
 cd..
 
-rem Запуск сборки vcpkg.json (x64/ARM64 Debug/Release)
-vcpkg\vcpkg.exe install --triplet "x64-windows-static" --x-install-root=vcpkg\installed\windows
-vcpkg\vcpkg.exe install --triplet "arm64-android" --x-install-root=vcpkg\installed\android
+if exist ..\vcpkg.json (
+  set ManifestRoot=--x-manifest-root=..
+)
+
+vcpkg\vcpkg.exe install --triplet "x64-windows-static-md" --x-install-root=vcpkg %ManifestRoot%
+if ERRORLEVEL 1 (
+  echo x64-windows: error: build failed
+  pause
+  goto EndOfFile
+)
+
+rem Копировать в одну общую папку нельзя, .т.к некоторые заголовочные файлы
+rem Windows и Android версий отличаются.
+xcopy "vcpkg\x64-windows-static-md\*" "Externals\Windows" /S /Q /Y /I
+
+vcpkg\vcpkg.exe install --triplet "arm64-android" --x-install-root=vcpkg %ManifestRoot%
+if ERRORLEVEL 1 (
+  echo arm64-android: error: build failed
+  pause
+  goto EndOfFile
+)
+
+rem Копировать в одну общую папку нельзя, .т.к некоторые заголовочные файлы
+rem Windows и Android версий отличаются.
+xcopy "vcpkg\arm64-android\*" "Externals\Android" /S /Q /Y /I
+
+del .\vcpkg\buildtrees\*.* /S /Q
+
+rem Удачная сборка, добавить файл-флаг как маркер этого факта
+echo Success > .\Externals\Success.build
 
 rem Библиотеки собираются в \packages\...
 rem Результат копируется в \installed\...
+
+goto EndOfFile
+
+:CheckPathToAndroidNdk
+
+if not "%~nx1" == "%EXPECTED_ANDROID_NDK%" (
+  echo ANDROID_NDK_HOME: error: expected path to %EXPECTED_ANDROID_NDK%
+  pause
+  exit /b 1
+)
+
+exit /b 0
 
 :EndOfFile
